719d33d519a4afb71d58a561988385f7a36f9937
nguye <nguyentrannhatminh@gmail.com>
2020-02-03 23:21:08 +0700

    add created in payment and paymentTransaction.

--- banking-platform/src/main/java/com/hcmus/banking/platform/core/presentation/paymentTransaction/PaymentTransactionResponse.java
+++ banking-platform/src/main/java/com/hcmus/banking/platform/core/presentation/paymentTransaction/PaymentTransactionResponse.java
@@ -1,17 +1,22 @@
 package com.hcmus.banking.platform.core.presentation.paymentTransaction;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hcmus.banking.platform.domain.paymentTransaction.PaymentTransaction;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class PaymentTransactionResponse {
     public String code;
     public BigDecimal money;
     public String content;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    public LocalDateTime createdAt;
 
     public PaymentTransactionResponse(PaymentTransaction paymentTransaction) {
         this.code = paymentTransaction.getCode();
         this.money = paymentTransaction.getMoney();
         this.content = paymentTransaction.getContent();
+        this.createdAt = paymentTransaction.getCreated().getCreatedAt().getValue();
     }
 }
