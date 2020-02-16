fcd3cb10244efb0c3a46509a8199c5a1f1c5a22c
HaThiMyChi <mychikhtn@gmail.com>
2020-02-16 22:50:23 +0700

    Commit Add employee pages

---
+++ banking-ui/src/app/employee-pages/employee-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-employee-pages',
+  templateUrl: './employee-pages.component.html',
+  styleUrls: ['./employee-pages.component.scss']
+})
+export class EmployeePagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ banking-ui/src/app/employee-pages/employee-pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeePagesRoutingModule } from './employee-pages-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmployeePagesRoutingModule
+  ]
+})
+export class EmployeePagesModule { }
