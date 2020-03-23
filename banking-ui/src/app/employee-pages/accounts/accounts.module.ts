893b518763e7d66febe7e2927ab2e4f8813da9c4
Nguyen Minh Phong <nmphong0601@gmail.com>
2020-03-23 18:09:35 +0700

    Commit update Add customer component

--- banking-ui/src/app/employee-pages/accounts/accounts.module.ts
+++ banking-ui/src/app/employee-pages/accounts/accounts.module.ts
@@ -6,6 +6,50 @@ import { AccountsComponent } from './accounts.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+
+/**
+ * Custom angular notifier options
+ */
+const customNotifierOptions: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: 'left',
+      distance: 12
+    },
+    vertical: {
+      position: 'top',
+      distance: 12,
+      gap: 10
+    }
+  },
+  theme: 'material',
+  behaviour: {
+    autoHide: 3000,
+    onClick: false,
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+    preset: 'fade',
+    speed: 300,
+    easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    }
+  }
+};
 
 import {
   NbDatepickerModule,
@@ -59,6 +103,7 @@ import { StatusCardComponent } from './status-card/status-card.component';
     NbWindowModule,
     AccountsRoutingModule,
     NbCheckboxModule,
+    NotifierModule.withConfig(customNotifierOptions)
   ]
 })
 export class AccountsModule { }
--- banking-ui/src/app/employee-pages/accounts/new-account/new-account.component.ts
+++ banking-ui/src/app/employee-pages/accounts/new-account/new-account.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CustomerService } from '../../../_services/customer.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NbDateService } from '@nebular/theme';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'ngx-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.scss']
 })
-export class NewAccountComponent implements OnInit {
+export class NewAccountComponent implements OnInit, OnDestroy {
 
+  private readonly notifier: NotifierService;
   customerForm: FormGroup;
   maxBirthDate: Date;
 
-  constructor(private customerService: CustomerService,
+  constructor(private notifications: NotifierService,
+              private customerService: CustomerService,
               private fb: FormBuilder,
               private dateService: NbDateService<Date>) {
+              this.notifier = notifications;
               this.maxBirthDate = this.dateService.createDate(this.dateService.today().getFullYear() - 18,0,1);
   }
 
@@ -28,16 +34,33 @@ export class NewAccountComponent implements OnInit {
       gender: "Male",
       phone: "",
       address: "",
-      email: ""
+      email: ["", Validators.required]
     });
   }
 
   onSubmit(formData) {
-
+    this.customerService.Add(formData).pipe(untilDestroyed(this))
+    .subscribe(
+      (success: any) => {
+        this.notifier.show({
+          type: "success",
+          message: "Đăng ký thành công!",
+          id: "register-success"
+        });
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.show({
+          type: "error",
+          message: `Đăng ký thất bại! ${error}`,
+          id: "register-error"
+        });
+      }
+    );
   }
 
   ngOnInit() {
     this.createForm();
   }
 
+  ngOnDestroy(){}
 }
